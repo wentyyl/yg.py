09fb529c6b564d1bacc17e202f6592779a0bdb52
wentyyl <166291573+wentyyl@users.noreply.github.com>
2024-04-07 14:49:57 +0500

    Create asdf.py

---
+++ asdf.py
@@ -0,0 +1,14 @@
+perviu = int(input('Введите оценку за первый предмет')
+vtoroy = int(input('Введите оценку за второй предмет'))
+treti = int(input('Введите оценку за третий предмет))
+chetvert = int(input('Введите оценку за четвёртый предмет'))
+pyati = int(input('Введите оценку за пятый предмет'))
+avg = (perviu + voroy + treti + chetvert + pyati)/5
+if (avg >= 90)
+  print('Отлично')
+elif (avg >= 80 and avg < 90):
+  print(Хорошо')
+elif (avg >= 70) and avg < 80:
+  print('Нормально')
+elif (avg >=60 and avg < 70):
+  prit('Незачёт')
